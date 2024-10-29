f8a171d1889b49273776871688f9cb03ece4d575
weisro-source <nassimsolimamalgo@gmail.com>
2024-10-29 17:34:07 +0200

    refactoring the onborading page

---
+++ lib/feature/onboarding/data/onboarding_image.dart
@@ -0,0 +1,12 @@
+import 'package:weisro/core/assets_path/image_path.dart';
+
+abstract class OnBoardingImage {
+  
+  static final List<String> imageList = [
+    ImagePath.imagesOnboardingImage1,
+    ImagePath.imagesOnboardingImage2,
+    ImagePath.imagesOnboardingImage3,
+  ];
+
+  
+}
--- lib/feature/onboarding/presentation/view/pages/on_boarding_page_view_body.dart
+++ lib/feature/onboarding/presentation/view/pages/on_boarding_page_view_body.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gallery_3d/gallery3d.dart';
-
-import 'package:weisro/core/assets_path/image_path.dart';
-import 'package:weisro/core/styles/app_style.dart';
 import 'package:weisro/core/utils/helper_functions.dart';
 import 'package:weisro/core/utils/sized_box_extension.dart';
-import 'package:weisro/feature/onboarding/data/onboarding_sentences.dart';
+import 'package:weisro/feature/onboarding/data/onboarding_image.dart';
+import 'package:weisro/feature/onboarding/presentation/manager/onboarding_controller.dart';
 import 'package:weisro/feature/onboarding/presentation/view/pages/selected_account_type_view.dart';
 import 'package:weisro/feature/onboarding/presentation/view/widgets/gallery_3d_widget.dart';
 import 'package:weisro/feature/onboarding/presentation/view/widgets/page_indicator_widget.dart';
 
 import '../../../../../core/widgets/logo_image_widget.dart';
 import '../widgets/bottom_button.dart';
+import 'package:weisro/feature/onboarding/data/onboarding_sentences.dart';
+import 'package:weisro/core/styles/app_style.dart';
 
 class OnBoardingPageViewBody extends StatefulWidget {
   const OnBoardingPageViewBody({super.key});
@@ -22,56 +21,24 @@ class OnBoardingPageViewBody extends StatefulWidget {
 
 class _OnBoardingPageViewBodyState extends State<OnBoardingPageViewBody>
     with SingleTickerProviderStateMixin {
-  final List<String> imageUrlList = [
-    ImagePath.imagesOnboardingImage1,
-    ImagePath.imagesOnboardingImage2,
-    ImagePath.imagesOnboardingImage3,
-  ];
-
-  late final Gallery3DController _gallery3DController;
-  final ValueNotifier<int> currentIndexNotifier = ValueNotifier<int>(0);
-
-  late final AnimationController _animationController;
-  late final Animation<double> _fadeInAnimation;
-  late final Animation<double> _scaleAnimation;
+  late final OnboardingController _onboardingController;
 
   @override
   void initState() {
     super.initState();
-    _gallery3DController =
-        Gallery3DController(itemCount: imageUrlList.length, autoLoop: false);
-
-    currentIndexNotifier.addListener(() {
-      if (currentIndexNotifier.value == 2) {
-        Future.delayed(const Duration(seconds: 1))
-            .then((value) => _navigateToHomePage());
-      }
-    });
-
-    _animationController = AnimationController(
+    _onboardingController = OnboardingController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      itemCount: OnBoardingImage.imageList.length,
+      onItemChanged: (index) {
+        _onboardingController.currentIndexNotifier.value = index;
+      },
+      onLastIndexReached: _navigateToHomePage,
     );
-
-    _fadeInAnimation = CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeIn,
-    );
-
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeOut,
-      ),
-    );
-
-    _animationController.forward();
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
-    currentIndexNotifier.dispose();
+    _onboardingController.dispose();
     super.dispose();
   }
 
@@ -85,14 +52,14 @@ class _OnBoardingPageViewBodyState extends State<OnBoardingPageViewBody>
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          104.kh,
+          50.kh,
           AnimatedBuilder(
-            animation: _animationController,
+            animation: _onboardingController.animationController,
             builder: (context, child) {
               return Opacity(
-                opacity: _fadeInAnimation.value,
+                opacity: _onboardingController.fadeInAnimation.value,
                 child: Transform.scale(
-                  scale: _scaleAnimation.value,
+                  scale: _onboardingController.scaleAnimation.value,
                   child: child,
                 ),
               );
@@ -102,38 +69,40 @@ class _OnBoardingPageViewBodyState extends State<OnBoardingPageViewBody>
           28.kh,
           Center(
             child: ValueListenableBuilder<int>(
-              valueListenable: currentIndexNotifier,
+              valueListenable: _onboardingController.currentIndexNotifier,
               builder: (context, value, _) {
-                return Text(
-                  OnboardingSentences.onboardingSentences(context)[value],
-                  style: AppStyles.style16w400Grey(context),
-                  textAlign: TextAlign.center,
+                return Padding(
+                  padding: const EdgeInsetsDirectional.symmetric(horizontal: 5),
+                  child: Text(
+                    OnboardingSentences.onboardingSentences(context)[value],
+                    style: AppStyles.style16w400Grey(context),
+                    textAlign: TextAlign.center,
+                  ),
                 );
               },
             ),
           ),
           71.kh,
           Gallery3DWidget(
-            controller: _gallery3DController,
-            imageUrlList: imageUrlList,
+            controller: _onboardingController.gallery3DController,
+            imageUrlList: OnBoardingImage.imageList,
             onItemChanged: (index) {
-              currentIndexNotifier.value = index;
+              _onboardingController.currentIndexNotifier.value = index;
             },
           ),
           ValueListenableBuilder<int>(
-            valueListenable: currentIndexNotifier,
+            valueListenable: _onboardingController.currentIndexNotifier,
             builder: (context, currentIndex, _) {
               return PageIndicatorWidget(
                 controller: PageController(initialPage: currentIndex),
-                count: imageUrlList.length,
+                count: OnBoardingImage.imageList.length,
               );
             },
           ),
-          137.kh,
           Padding(
             padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
             child: BottomButton(
-              currentIndexNotifier: currentIndexNotifier,
+              currentIndexNotifier: _onboardingController.currentIndexNotifier,
               onPressedOnSkip: _navigateToHomePage,
             ),
           ),
