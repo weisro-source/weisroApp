1d525bc7904dc4310863022da6de667c07fb49b1
weisro-source <nassimsolimamalgo@gmail.com>
2025-01-18 13:22:31 +0200

    add full screen image

--- lib/core/widgets/image_list_in_details_page.dart
+++ lib/core/widgets/image_list_in_details_page.dart
@@ -6,6 +6,7 @@ import 'package:shimmer/shimmer.dart';
 import 'package:weisro/core/assets_path/image_path.dart';
 import 'package:weisro/core/styles/app_color.dart';
 import 'package:weisro/core/utils/helper_functions.dart';
+import 'package:weisro/core/widgets/full_screen_image.dart';
 
 class ImageListInDetailsPage extends StatelessWidget {
   const ImageListInDetailsPage({
@@ -27,48 +28,62 @@ class ImageListInDetailsPage extends StatelessWidget {
         itemCount: imageList.length,
         itemBuilder: (context, index) {
           String oneImageUrl = imageList[index];
-          return Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(4),
-                child: isReview
-                    ? Image.file(
-                        File(oneImageUrl),
-                        width: 341,
-                        height: 220,
-                        fit: BoxFit.cover,
-                      )
-                    : CachedNetworkImage(
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) {
-                          return Shimmer.fromColors(
-                            baseColor: AppColors.shimmerBaseColor,
-                            highlightColor: AppColors.shimmerHighlightColor,
-                            child: Container(
-                              width: 341,
-                              height: 220,
-                              decoration: const BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(4))),
-                            ),
-                          );
-                        },
-                        width: 341,
-                        height: 220,
-                        imageUrl:
-                            HelperFunctions.addImageNameForUrl(oneImageUrl),
-                        errorWidget: (context, url, error) {
-                          return AspectRatio(
-                            aspectRatio: 1.55,
-                            child: Image.asset(
-                              ImagePath.imagesService3,
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        },
-                      ),
-              )
-            ],
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => FullScreenImageGallery(
+                    imageList: imageList,
+                    initialIndex: index,
+                    isReview: isReview,
+                  ),
+                ),
+              );
+            },
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: isReview
+                      ? Image.file(
+                          File(oneImageUrl),
+                          width: 341,
+                          height: 220,
+                          fit: BoxFit.cover,
+                        )
+                      : CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) {
+                            return Shimmer.fromColors(
+                              baseColor: AppColors.shimmerBaseColor,
+                              highlightColor: AppColors.shimmerHighlightColor,
+                              child: Container(
+                                width: 341,
+                                height: 220,
+                                decoration: const BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(4))),
+                              ),
+                            );
+                          },
+                          width: 341,
+                          height: 220,
+                          imageUrl:
+                              HelperFunctions.addImageNameForUrl(oneImageUrl),
+                          errorWidget: (context, url, error) {
+                            return AspectRatio(
+                              aspectRatio: 1.55,
+                              child: Image.asset(
+                                ImagePath.imagesService3,
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          },
+                        ),
+                )
+              ],
+            ),
           );
         },
       ),
